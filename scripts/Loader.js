b347cc06aa8c69579f2deb7dce46afe75320942d
Alex Curran <alexcurran.va@gmail.com>
2020-12-01 09:48:21 -0600

    Moved loader component into separate module file

---
+++ scripts/Loader.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const loaderModule = (() => {
+    // DOM elem ref
+    const loader = document.querySelector(".loading");
+
+    function showLoader() {
+        if (loader.attributeStyleMap) {
+            // Use CSS Houdini Typed OM
+            loader.attributeStyleMap.set("display", "flex");
+        } else {
+            // Use old 'style' property
+            loader.style.display = "flex";
+        }
+    }
+
+    function hideLoader() {
+        if (loader.attributeStyleMap) {
+            loader.attributeStyleMap.clear();
+        } else {
+            loader.style.display = "";
+        }
+    }
+
+    return {
+        showLoader,
+        hideLoader
+    };
+})();
+
+export { loaderModule };
--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,6 @@
 "use strict";
 
-const loaderModule = (() => {
-    // DOM elem ref
-    const loader = document.querySelector(".loading");
-
-    function showLoader() {
-        if (loader.attributeStyleMap) {
-            // Use CSS Houdini Typed OM
-            loader.attributeStyleMap.set("display", "flex");
-        } else {
-            // Use old 'style' property
-            loader.style.display = "flex";
-        }
-    }
-
-    function hideLoader() {
-        if (loader.attributeStyleMap) {
-            loader.attributeStyleMap.clear();
-        } else {
-            loader.style.display = "";
-        }
-    }
-
-    return {
-        showLoader,
-        hideLoader
-    };
-})();
+import { loaderModule } from "./Loader.js";
 
 const paginationModule = (() => {
     // Data
