1ff7ff7003c1fd1e514a70d2f469be34d763895d
Alex Curran <alexcurran.va@gmail.com>
2021-03-31 16:49:25 -0500

    Replaced JS-based first-letter-capitalization with a CSS-only version, using ::first-letter and span elems

--- scripts/main.js
+++ scripts/main.js
@@ -210,11 +210,11 @@ const pokemonPopupModule = (() => {
                     <section class="poke-main-section-data">
                         <h1 class="poke-main-section-data-title">${pokeCardData.name}</h1>
                         <img class="poke-main-section-data-img" src="https://pokeres.bastionbot.org/images/pokemon/${pokeCardData.id}.png" alt="${pokeCardData.name} Pokemon">
-                        <p class="poke-main-section-data-body">Type: ${capInitialLetter(pokeType)}</p>
+                        <p class="poke-main-section-data-body">Type: <span class="cap-first">${pokeType}</span></p>
                         <p class="poke-main-section-data-body">Weight: ${convertedWeight} lbs</p>
                         <p class="poke-main-section-data-body">Height: ${convertedHeight} ft</p>
                         <div class="poke-main-section-data-abilities">
-                            <p class="poke-main-section-data-abilities-title">Ability: <span>${capInitialLetter(pokeCardData.abilities[0].ability.name)}</span></p>
+                            <p class="poke-main-section-data-abilities-title">Ability: <span class="cap-first">${pokeCardData.abilities[0].ability.name}</span></p>
                         </div>
                     </section>
                     <section class="poke-main-section-stats">
@@ -248,11 +248,6 @@ const pokemonPopupModule = (() => {
         `;
     
         main.insertAdjacentHTML("afterbegin", pokeCardHTML);
-    }
-
-    // Helper func
-    function capInitialLetter(str) {
-        return str[0].toUpperCase() + str.slice(1);
     }
 
     function handleClosePopopClick(event) {
