477dbe3134fde582b1c3f7de63c28dead0ee1b65
Alex Curran <alexcurran.va@gmail.com>
2021-10-25 15:08:45 -0500

    Add try/catch block to async func

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,26 @@ import { Loader } from "./Loader.js";
 
 // On page load
 async function init() {
-    const POKEMON_PER_PAGE = 50;
-    const apiEndpoint = `https://pokeapi.co/api/v2/pokemon?limit=${POKEMON_PER_PAGE}`;
+    try {
+        const POKEMON_PER_PAGE = 50;
+        const apiEndpoint = `https://pokeapi.co/api/v2/pokemon?limit=${POKEMON_PER_PAGE}`;
+        
+        Loader.showLoader();
     
-    Loader.showLoader();
-
-    // Get Pokemon data
-    const { pokemonData, paginationUrlNext, paginationUrlPrev } = await PokemonCardsGrid.getPokemonData(apiEndpoint);
-    // Create Pokemon cards
-    PokemonCardsGrid.createAllPokemon(pokemonData);
-
-    Loader.hideLoader();
-
-    // Update pagination data
-    Pagination.updatePaginationUrl("nextUrl", paginationUrlNext);
-    Pagination.updatePaginationUrl("prevUrl", paginationUrlPrev);
+        // Get Pokemon data
+        const { pokemonData, paginationUrlNext, paginationUrlPrev } = await PokemonCardsGrid.getPokemonData(apiEndpoint);
+        // Create Pokemon cards
+        PokemonCardsGrid.createAllPokemon(pokemonData);
+    
+        Loader.hideLoader();
+    
+        // Update pagination data
+        Pagination.updatePaginationUrl("nextUrl", paginationUrlNext);
+        Pagination.updatePaginationUrl("prevUrl", paginationUrlPrev);
+        
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 init();
