130ddf138fa2ea203b9a7e6751e82f15c583cf4a
Alex Curran <alexcurran.va@gmail.com>
2021-10-27 16:55:18 -0500

    Improve search filter functions by replacing regex with includes string method, also replace style property JS edits with class add/remove to trigger hiding or showing cards

--- scripts/SearchFilter.js
+++ scripts/SearchFilter.js
@@ -3,34 +3,21 @@ const searchInput = form.elements.input;
 
 // Helper functions
 function showCard(card) {
-    if (card.attributeStyleMap) {
-        // Use CSS Houdini Typed OM
-        card.attributeStyleMap.clear();
-    } else {
-        // Keep original "display: flex" value
-        card.style.display = "";
-    }
+    card.classList.remove("card--hide");
 }
 
 function hideCard(card) {
-    if (card.attributeStyleMap) {
-        // Use CSS Houdini Typed OM
-        card.attributeStyleMap.set("display", "none");
-    } else {
-        // Fallback to old "style" property
-        card.style.display = "none";
-    }
+    card.classList.add("card--hide");
 }
 
 function getMatches() {
     const pokeCards = document.querySelectorAll(".main-card-btn-container");
-    const searchTerm = searchInput.value;
-    const regex = new RegExp(`^${searchTerm}`, "gi"); // Match the title beginning with searchTerm
+    const searchTermLower = searchInput.value.toLowerCase();
 
     for (const card of pokeCards) {
         const title = card.querySelector(".main-card-content-name").textContent;
         
-        if (title.match(regex)) {
+        if (title.includes(searchTermLower)) {
             showCard(card);
         } else {
             hideCard(card);
