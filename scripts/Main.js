b4fd8573cf6c7b79c7dc7c845598ce50df046853
Alex Curran <alexcurran.va@gmail.com>
2023-03-28 15:17:33 -0500

    Remove unneeded comments

--- scripts/Main.js
+++ scripts/Main.js
@@ -2,17 +2,13 @@ import { getPokemonData, createAllPokemon } from "./PokemonCardsGrid.js";
 import { paginationNextBtn, paginationPrevBtn, paginationBtnHideClass, updatePaginationUrl, togglePaginationBtnVisibility } from "./Pagination.js";
 import { showLoader, hideLoader } from "./Loader.js";
 
-// On page load
 async function init() {
     try {
         const POKEMON_PER_PAGE = 50;
         const apiEndpoint = `https://pokeapi.co/api/v2/pokemon?limit=${POKEMON_PER_PAGE}`;
         
         showLoader();
-    
-        // Get Pokemon data
         const { pokemonData, paginationUrlNext, paginationUrlPrev } = await getPokemonData(apiEndpoint);
-        // Create Pokemon cards
         createAllPokemon(pokemonData);
     
         // Update pagination data
