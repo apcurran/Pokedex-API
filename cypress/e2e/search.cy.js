57168b0010ac4fca29d1b69926292b570b3d1d5d
Alex Curran <alexcurran.va@gmail.com>
2025-12-03 16:56:25 -0600

    Search box functionality test with happy, correct path

---
+++ cypress/e2e/search.cy.js
@@ -0,0 +1,35 @@
+describe("Search box functionality", () => {
+    const searchInput = "input.home-form-search";
+    const pokemonPopup = "#pokemon-popup";
+    const error = ".home-error";
+
+    beforeEach(() => {
+        cy.intercept(
+            "GET",
+            "https://pokeapi.co/api/v2/pokemon?offset=0&limit=50",
+        ).as("getPokemonCharactersData");
+
+        cy.visit("/");
+
+        cy.wait("@getPokemonCharactersData");
+    });
+
+    it("should successfully search a pokemon character and render the popup with the character's data", () => {
+        const pokemonCharacterName = "mew";
+
+        cy.intercept(
+            "GET",
+            `https://pokeapi.co/api/v2/pokemon/${pokemonCharacterName}`,
+        ).as("getCharacterData");
+
+        cy.get(searchInput).type(`${pokemonCharacterName}{enter}`);
+
+        cy.wait("@getCharacterData");
+
+        cy.get(pokemonPopup).should("be.visible");
+
+        cy.get("h2").should("contain.text", pokemonCharacterName);
+
+        cy.get(error).should("not.have.class", "home-error--show");
+    });
+});
