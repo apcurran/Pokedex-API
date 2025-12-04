a077152f14780871ee64556c20ac750abf6e730e
Alex Curran <alexcurran.va@gmail.com>
2025-12-04 14:47:56 -0600

    TEST: Navigation forwards and backwards with pagination buttons works

--- cypress/e2e/pagination.cy.js
+++ cypress/e2e/pagination.cy.js
@@ -32,9 +32,34 @@ describe("Pagination functionality", () => {
 
         cy.get(cards)
             .first()
-            .should("not.contain", /bulbasaur/);
+            .should("not.contain", /bulbasaur/i);
 
         cy.get(nextButton).should("not.have.class", cardHideClass);
         cy.get(previousButton).should("not.have.class", cardHideClass);
+    });
+
+    it("should navigate back to the first page, and navigation previous should be hidden", () => {
+        cy.intercept("GET", "**/api/v2/pokemon?offset=50&limit=50").as(
+            "pageTwoLoad",
+        );
+        cy.intercept("GET", "**/api/v2/pokemon?offset=0&limit=50").as(
+            "pageOneLoad",
+        );
+
+        // move to second page
+        cy.get(nextButton).click();
+        cy.wait("@pageTwoLoad");
+        cy.get(previousButton).should("not.have.class", cardHideClass);
+        cy.get(previousButton).click();
+        cy.wait("@pageOneLoad");
+
+        cy.get(cards).should("have.length", POKEMON_COUNT);
+
+        cy.get(cards)
+            .first()
+            .contains(/bulbasaur/i);
+
+        cy.get(nextButton).should("not.have.class", cardHideClass);
+        cy.get(previousButton).should("have.class", cardHideClass);
     });
 });
